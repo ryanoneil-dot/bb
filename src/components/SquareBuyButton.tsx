5f15c929a22439f41feb52a1ff39b67b87a920ef
a <a>
2026-01-19 01:02:19 +0000

    feat: update Square buy button embed style

--- src/components/SquareBuyButton.tsx
+++ src/components/SquareBuyButton.tsx
@@ -8,25 +8,43 @@ type Props = {
 export default function SquareBuyButton({ href, label }: Props) {
   const url = href || 'https://square.link/u/0qjenOxy?src=embed'
   return (
-    <a
-      target="_blank"
-      rel="noopener noreferrer"
-      href={url}
+    <div
       style={{
-        display: 'inline-block',
-        fontSize: 18,
-        lineHeight: '48px',
-        height: 48,
-        color: '#ffffff',
-        minWidth: 212,
-        backgroundColor: '#006aff',
-        textAlign: 'center',
-        boxShadow: '0 0 0 1px rgba(0,0,0,.1) inset',
-        borderRadius: 6,
-        textDecoration: 'none',
+        overflow: 'auto',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        width: 259,
+        background: '#FFFFFF',
+        border: '1px solid rgba(0, 0, 0, 0.1)',
+        boxShadow: '-2px 10px 5px rgba(0, 0, 0, 0)',
+        borderRadius: 10,
+        fontFamily: 'SQ Market, SQ Market, Helvetica, Arial, sans-serif',
       }}
     >
-      {label || 'Buy now'}
-    </a>
+      <div style={{ padding: 20 }}>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href={url}
+          style={{
+            display: 'inline-block',
+            fontSize: 18,
+            lineHeight: '48px',
+            height: 48,
+            color: '#ffffff',
+            minWidth: 212,
+            backgroundColor: '#006aff',
+            textAlign: 'center',
+            boxShadow: '0 0 0 1px rgba(0,0,0,.1) inset',
+            borderRadius: 6,
+            textDecoration: 'none',
+          }}
+        >
+          {label || 'Buy now'}
+        </a>
+      </div>
+    </div>
   )
 }
